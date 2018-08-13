9997ca8a430c3828ee99a8544eb588105936ae22
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-14 03:02:05 +0800

    [Exercise Review] Using Closures

--- closures/exercise.groovy
+++ closures/exercise.groovy
@@ -24,6 +24,7 @@ def fibs = [1, 1, 2, 3, 5, 8, 13, 21]
 def isEven = { it % 2 == 0 }
 assert fibs.find(isEven) == 2
 assert fibs.findAll(isEven) == [2, 8]
+assert fibs.find { it >= 100 } == null
 
 assert fibs.any(isEven)
 assert !fibs.every(isEven)
@@ -31,4 +32,3 @@ assert !fibs.every(isEven)
 assert ["Clubs", "Diamonds", "Spades", "Hearts"].groupBy { it.length() } == [5:["Clubs"], 8:["Diamonds"], 6:["Spades", "Hearts"]]
 assert ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"].groupBy { it.substring(0, 1) } ==
         [S:["Sunday", "Saturday"], M:["Monday"], T:["Tuesday", "Thursday"], W:["Wednesday"], F:["Friday"]]
-
